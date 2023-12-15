529fc8c1e8e2977eabd0f4075d0e69d2a3d7191a
nonso-chiagunye <nonsoasoby@hotmail.com>
2023-12-15 20:10:02 +0400

    Corrected SG

--- instance.tf
+++ instance.tf
@@ -4,72 +4,29 @@ resource "aws_kms_key" "test-kms" {
   deletion_window_in_days = 7
 }
 
-# data "aws_ami" "ubuntu" {
-#     most_recent = true
+resource "aws_security_group" "test-sg" {
+  vpc_id      = "vpc-6ffe7f04"
+  name        = "test-sg"
+  description = "test security group"
 
-#     filter {
-#         name   = "name"
-#         values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#     }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#     filter {
-#         name   = "virtualization-type"
-#         values = ["hvm"]
-#     }
-
-#     owners = ["099720109477"] # Canonical
-# }
-
-# resource "aws_instance" "jenkins-server" {
-#   # ami           = data.aws_ami.ubuntu.id
-#   ami           = "ami-06d4b7182ac3480fa"
-#   instance_type = "t2.micro"
-#   # key_name      = var.KEY_NAME
-#   # iam_instance_profile = aws_iam_instance_profile.jenkins-server-instance-profile.name
-
-#   vpc_security_group_ids = [aws_security_group.test-sg.id]
-#   subnet_id = "subnet-0088c31ea8f945b10"
-#   # user_data = file("jenkins.sh")
-
-#   root_block_device {
-#     delete_on_termination = true 
-#     volume_type = "gp2"
-#     volume_size = "10"
-#     # iops = var.volume_type == "gp2" ? null : var.volume_iops
-#     # throughput = var.volume_type != "gp3" ? null : var.throughput
-#     encrypted = true 
-#     kms_key_id = aws_kms_key.test-kms.id 
-#   }
-
-#   tags = {
-#     Name = "test-kms"
-#   }
-# }
-
-# resource "aws_instance" "jump-server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.micro"
-#   key_name      = var.KEY_NAME
-#   vpc_security_group_ids = [aws_security_group.jump-server-sg.id]
-#   subnet_id = aws_subnet.jenkins-public.id
-
-#   provisioner "file" {
-#     source = "${var.path_to_key}/${var.KEY_NAME}.pem"
-#     destination = var.key_destination   
-#   }  
-
-#   connection {
-#     host        = coalesce(self.public_ip, self.private_ip)
-#     type        = "ssh"
-#     user        = "ubuntu"
-#     private_key = file("${var.path_to_key}/${var.KEY_NAME}.pem")
-#   }
-
-#   tags = {
-#     Name = "jump-server"
-#   }
-# }
-
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["91.73.64.16/32"]
+  }
+  
+  tags = {
+    Name = "test-sg"
+  }
+}
 
 resource "aws_instance" "test-server" {
   # ami           = data.aws_ami.ubuntu.id
